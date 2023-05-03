1a1a75b6eb29450234344229a620acadbfb234f7
Edvan Ribeiro <jk.jr47@gmail.com>
2023-05-03 10:27:30 -0300

    update

---
+++ tic-tac-toe-game/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Board from "./components/Board";
+
+export default function App() {
+  return (
+    <Board/>
+  );
+}
---
+++ tic-tac-toe-game/src/components/Square.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+
+
+export function Square({value, onSquareClick}) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
