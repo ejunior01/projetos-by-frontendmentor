646a104137fb8cd70e7c7a9f3b972160087f9a7d
ejunior01 <jk.jr47@gmail.com>
2024-07-05 00:51:58 -0300

    Melhorado a estilização e responsividade, e reorganizado os elementos html

--- article-preview-component-master/js/script.js
+++
@@ -1,9 +0,0 @@
-
-const share = document.querySelector(".share")
-const shareSocial = document.querySelector(".share-social")
-
-
-share.addEventListener('click', ()=> {
-    share.classList.toggle('rotate')
-    shareSocial.classList.toggle('active')
-})
